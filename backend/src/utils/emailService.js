89d96efb67a49b1a586d4aec6294d2cb405a5d4d
celvios <toluking001@gmail.com>
2025-09-13 08:47:30 +0100

    Fix Gmail SMTP configuration

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -2,9 +2,7 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
